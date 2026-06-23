755a8ff29aab66e901b00f098464d0dbe7623503
Prashant5551 <prashkbk@gmail.com>
2026-06-23 21:12:01 +0530

    Angular 22 Data Binding

--- learning_scenario_based/src/app/Angular_22/data-binding/data-binding.ts
+++ learning_scenario_based/src/app/Angular_22/data-binding/data-binding.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+
+  productName: string = 'Headphone';
+  productSize: number = 12000;
+  maxAllowedChar: number = 5;
+  isModelActive: boolean = false;
+
+  myDynamictype = 'Button';
+
+  showWelcometext() {
+    alert("Welcome version 22");
+  }
+
+  onDropdownChange() {
+    alert("Dropdown value changed")
+  }
+
+  onMouseEnter() {
+    console.log('Mouse entered');
+  }
+
+  onMouseLeft() {
+    console.log('Mouse left Div');
+  }
+}
