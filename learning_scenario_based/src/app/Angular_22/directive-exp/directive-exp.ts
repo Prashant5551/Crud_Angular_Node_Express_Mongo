a7b7d6c5000874a6b596191cbb3d0f97fe4ae97c
Prashant5551 <prashkbk@gmail.com>
2026-06-24 21:50:42 +0530

    Angular 22 Directive code

--- learning_scenario_based/src/app/Angular_22/directive-exp/directive-exp.ts
+++ learning_scenario_based/src/app/Angular_22/directive-exp/directive-exp.ts
@@ -1,9 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directive-exp',
-  imports: [],
+  imports: [NgClass, FormsModule],
   templateUrl: './directive-exp.html',
   styleUrl: './directive-exp.css',
 })
-export class DirectiveExp {}
+export class DirectiveExp {
+  //structural
+  //attribute
+  //custom
+  //component
+  //Directive - create/modify behaviour of element
+
+  divClassName: string = 'bg-success';
+  isCheckActive: boolean = false;
+
+  addDiv1Class(className: string) {
+    this.divClassName = className;
+  }
+}
