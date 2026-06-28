c7403b385eb1e0bf837911ef7afc52da6f93315a
Prashant5551 <prashkbk@gmail.com>
2026-06-28 21:30:35 +0530

    angular 22 signal form

--- learning_scenario_based/src/app/Angular_22/signal-form-ex/signal-form-ex.ts
+++ learning_scenario_based/src/app/Angular_22/signal-form-ex/signal-form-ex.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { form, FormField, minLength, required, schema } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-signal-form-ex',
-  imports: [],
+  imports: [FormField],
   templateUrl: './signal-form-ex.html',
   styleUrl: './signal-form-ex.css',
 })
-export class SignalFormEx {}
+export class SignalFormEx {
+
+  employeeModel = signal({
+    empName: '',
+    empCity: '',
+    empState: ''
+  })
+
+  employeeForm = form(this.employeeModel,(schema)=>{
+    required(schema.empName, {message:'name is required'}),
+    required(schema.empCity,{message:'city is required'}),
+    minLength(schema.empName,4,{message:'min 4 char needed'})
+  });
+
+  saveEmp() {
+    const formValue = this.employeeForm().value();
+    console.log(formValue)
+  }
+}
